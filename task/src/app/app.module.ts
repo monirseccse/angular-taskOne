b3d522cc159ebf3d33acdab52af032366b84d884
monir <monirantor7@gmail.com>
2023-03-14 09:08:25 +0600

    in progress

--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { Routes,RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 const appRoutes:Routes=[
-{path:'',component:HomeComponent},
+{path:'',redirectTo:'Home',pathMatch: 'full'},
 {path:'Home',component:HomeComponent},
 {path:'About',component:AboutComponent},
-{path:'Contact',component:ContactComponent}
+{path:'Contact',component:ContactComponent},
+{path:'**',component:ErrorComponent}
 ]
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ const appRoutes:Routes=[
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
