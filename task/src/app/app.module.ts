610f397473f73f8a46afdb7fe13beb854fdad073
monir <monirantor7@gmail.com>
2023-03-14 10:59:18 +0600

    grand child working

--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,12 +11,38 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { Routes,RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { PersonalComponent } from './personal/personal.component';
+import { GrandchildAComponent } from './grandchild-a/grandchild-a.component';
+import { GrandChildBComponent } from './grand-child-b/grand-child-b.component';
+import { GrandchildA2Component } from './grandchild-a2/grandchild-a2.component';
+import { GrandChildB2Component } from './grand-child-b2/grand-child-b2.component';
 
 const appRoutes:Routes=[
 {path:'',redirectTo:'Home',pathMatch: 'full'},
 {path:'Home',component:HomeComponent},
-{path:'About',component:AboutComponent},
+{
+  path:'About',component:AboutComponent
+},
+{path:'child-a',component:ChildAComponent,
+
+  children:[
+    {
+      path: 'grandchild-a', // child route path
+      component: GrandchildAComponent // child route component that the router renders
+    }
+  ]
+},
+{path:'child-b',component:ChildBComponent,
+children:[{
+  path:'grand-child-b',
+  component:GrandChildBComponent
+}]
+},
 {path:'Contact',component:ContactComponent},
+{path:'Personal',component:PersonalComponent},
 {path:'**',component:ErrorComponent}
 ]
 @NgModule({
@@ -27,7 +53,15 @@ const appRoutes:Routes=[
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    ErrorComponent
+    ErrorComponent,
+    SidebarComponent,
+    ChildAComponent,
+    ChildBComponent,
+    PersonalComponent,
+    GrandchildAComponent,
+    GrandChildBComponent,
+    GrandchildA2Component,
+    GrandChildB2Component
   ],
   imports: [
     BrowserModule,
