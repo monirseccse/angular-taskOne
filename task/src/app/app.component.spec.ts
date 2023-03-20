9460d7b8dccc136261ad384f0b3927c652b4d789
monir <monirantor7@gmail.com>
2023-03-20 10:20:38 +0600

    form added

--- task/src/app/app.component.spec.ts
+++ task/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task'`, () => {
+  it(`should have as title 'Task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task');
+    expect(app.title).toEqual('Task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('task app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Task app is running!');
   });
 });
--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -1,82 +1,20 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { Routes,RouterModule } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ChildAComponent } from './child-a/child-a.component';
-import { ChildBComponent } from './child-b/child-b.component';
-import { PersonalComponent } from './personal/personal.component';
-import { GrandchildAComponent } from './grandchild-a/grandchild-a.component';
-import { GrandChildBComponent } from './grand-child-b/grand-child-b.component';
-import { GrandchildA2Component } from './grandchild-a2/grandchild-a2.component';
-import { GrandChildB2Component } from './grand-child-b2/grand-child-b2.component';
+import { CardComponent } from './card/card.component';
 
-const appRoutes:Routes=[
-{path:'',redirectTo:'Home',pathMatch: 'full'},
-{path:'Home',component:HomeComponent},
-{
-  path:'About',component:AboutComponent
-},
-{path:'child-a',component:ChildAComponent,
-
-  children:[
-    {
-      path: 'grandchild-a', // child route path
-      component: GrandchildAComponent // child route component that the router renders
-    },
-    {
-      path:'grandchild-a2',
-      component:GrandchildA2Component
-    }
-  ]
-},
-{path:'child-b',component:ChildBComponent,
-children:[{
-  path:'grand-child-b',
-  component:GrandChildBComponent
-},
-{
-  path:'grand-child-b2',
-  component:GrandChildB2Component
-}
-]
-},
-{path:'Contact',component:ContactComponent},
-{path:'Personal',component:PersonalComponent},
-{path:'**',component:ErrorComponent}
-]
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    ErrorComponent,
-    SidebarComponent,
-    ChildAComponent,
-    ChildBComponent,
-    PersonalComponent,
-    GrandchildAComponent,
-    GrandChildBComponent,
-    GrandchildA2Component,
-    GrandChildB2Component
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
-    NgbModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
