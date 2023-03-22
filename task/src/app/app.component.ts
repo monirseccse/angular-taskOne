146678bbc272c6d3dd665ac244a0b792dca697b0
monir <monirantor7@gmail.com>
2023-03-22 21:22:15 +0600

    validation done

--- task/src/app/app.component.ts
+++ task/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component ,Input, OnInit} from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +11,19 @@ export class AppComponent implements OnInit{
   }
   title = 'Task';
   ValueOfTitle:string='';
-  Validate(evendta:any)
+  ValueOfDescription:string='';
+
+  OnSubmit(f:NgForm)
+  {
+console.log(f);
+  }
+  TitleValidate(evendta:any)
   {
     this.ValueOfTitle=(<HTMLInputElement>evendta.target).value;
 
+  }
+  DescriptionValidate(evendta:any)
+  {
+    this.ValueOfDescription=(<HTMLInputElement>evendta.target).value;
   }
 }
